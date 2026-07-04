dc9f3a1d24147febaab9d94398b970eaf74244a7
edisqc3-design <edisqc3@gmail.com>
2026-07-05 01:04:10 +0900

    Add files via upload

--- src/components/layout/BoardNavTabs.tsx
+++ src/components/layout/BoardNavTabs.tsx
@@ -1,7 +1,11 @@
 import { getBoards } from '@/lib/queries'
 import BoardNavTabsClient from './BoardNavTabsClient'
 
+// 상단 탭바에서는 노출하지 않을 게시판 (다른 메뉴에는 계속 노출됨)
+const HIDDEN_IN_TABBAR = ['공지사항', '자유게시판', '질문게시판', '정보게시판', '리뷰게시판']
+
 export default async function BoardNavTabs() {
   const boards = await getBoards()
-  return <BoardNavTabsClient boards={boards} />
+  const visibleBoards = boards.filter(b => !HIDDEN_IN_TABBAR.includes(b.name))
+  return <BoardNavTabsClient boards={visibleBoards} />
 }

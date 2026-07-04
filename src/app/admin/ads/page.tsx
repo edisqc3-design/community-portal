79a432ebac9d3fdcc14f10369b81b21fc40f8eaa
edisqc3-design <edisqc3@gmail.com>
2026-07-04 23:28:35 +0900

    Add files via upload

--- src/app/admin/ads/page.tsx
+++ src/app/admin/ads/page.tsx
@@ -1,1 +1,7 @@
+import { getAllSponsorAdsAdmin } from '@/lib/admin-queries'
+import AdsAdminClient from './AdsAdminClient'
 
+export default async function AdminAdsPage() {
+  const ads = await getAllSponsorAdsAdmin()
+  return <AdsAdminClient initialAds={ads} />
+}
